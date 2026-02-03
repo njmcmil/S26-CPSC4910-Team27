8275b800b391508e0de6e84ae4758e7aa0e379c6
jadeashley159 <47424189+jadeashley159@users.noreply.github.com>
2026-02-02 21:42:04 -0500

    Adding tokens

--- auth.py
+++ auth.py
@@ -20,6 +20,13 @@ Usage:
 """
 
 from passlib.hash import bcrypt
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from token_blacklist import is_token_blacklisted
+
+SECRET_KEY = "TFYSADGUH908746TTYGU4HRJFDT5367489TURHIGYUJDSHGFXCR567894JSVH672"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def hash_password(password: str) -> str:
     """
@@ -54,3 +61,22 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     except Exception:
         # invalid hash format or other error
         return False
+
+# create access token 
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+#verify token
+def verify_token(token: str):
+    # is it blacklisted? 
+    if is_token_blacklisted(token):
+        return None
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
