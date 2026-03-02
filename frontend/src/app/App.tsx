af250978cbacc685921331c3b194db511cdaa0e0
jadeashley159 <47424189+jadeashley159@users.noreply.github.com>
2026-03-02 15:59:55 -0500

    adding cart provider

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from '../auth/AuthContext';
+import { CartProvider } from '../auth/CartContext';
 import { router } from './routes';
 
 export function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <CartProvider>
+        <RouterProvider router={router} />
+      <CartProvider>
     </AuthProvider>
   );
 }
