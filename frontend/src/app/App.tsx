758dda6b7f4ce1171e519ec9aa5ad204b9108626
jadeashley159 <47424189+jadeashley159@users.noreply.github.com>
2026-03-02 16:16:43 -0500

    typo

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -8,7 +8,7 @@ export function App() {
     <AuthProvider>
       <CartProvider>
         <RouterProvider router={router} />
-      </CartProvider>CartProvider>
+      </CartProvider>
     </AuthProvider>
   );
 }
